e2a2128bedb933b582d7e3abb2b90bb8fabc233a
DESKTOP-IOTBAVS\David Elout <dedavid.elout@gmail.com>
2017-02-28 19:14:21 +0100

    Stap 4 done

--- Painter/Painter/GameWorld.cs
+++ Painter/Painter/GameWorld.cs
@@ -3,9 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.GamerServices;
+
+
 namespace Painter
 {
+    
     class GameWorld
     {
+        public Texture2D background;
+
+        ///////////////
+        public GameWorld(ContentManager contentmanager)
+        {
+            background = contentmanager.Load<Texture2D>("spr_background");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(background, Vector2.Zero, Color.White);
+            spriteBatch.End();
+        }
     }
+
+    
 }
--- Painter/Painter/Painter.cs
+++ Painter/Painter/Painter.cs
@@ -11,13 +11,12 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Painter
 {
-    /// <summary>
-    /// This is the main type for your game
-    /// </summary>
+    
     public class Painter : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        static GameWorld gameWorld;
 
         public Painter()
         {
@@ -25,12 +24,6 @@ namespace Painter
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -38,32 +31,12 @@ namespace Painter
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
-        protected override void LoadContent()
-        {
-            // Create a new SpriteBatch, which can be used to draw textures.
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
@@ -75,16 +48,18 @@ namespace Painter
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+            gameWorld = new GameWorld(this.Content);
+        }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
-
+            gameWorld.Draw(gameTime, spriteBatch);
             base.Draw(gameTime);
         }
     }
